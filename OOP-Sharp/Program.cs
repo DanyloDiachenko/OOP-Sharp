19e0ec0be7a61c060ff2ce352d5c91fe4b44adf2
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-03-10 18:04:06 +0200

    init fields

--- OOP-Sharp/Program.cs
+++ OOP-Sharp/Program.cs
@@ -12,27 +12,31 @@ namespace OOP_Sharp
 
     class Vacancy
     {
+        public string Title { get; set; }
 
     }
 
     class Category
     {
-
+        public string Title { get; set; }
     }
 
     class Resume
     {
-
+        public string Description { get; set; }
     }
 
     class Unemployed
     {
+        public string FirstName { get; set; }
+        /* public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; } */
 
-    }
-
-    class Agency
-    {
-        
+        class Agency
+        {
+            public string Name { get; set; }
+        }
     }
 }
 
